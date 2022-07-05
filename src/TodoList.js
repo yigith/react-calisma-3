fbd89d718925ddb1b8b5988b806703e1acb1f992
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2022-07-05 11:18:09 +0300

    crud

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,33 +4,68 @@ import React, { Component } from 'react';
 export class TodoList extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       baslik: "",
       yapilacaklar: [
-        { yapildiMi: false, baslik: "React Çalış"},
-        { yapildiMi: false, baslik: "Konsere Git"},
-        { yapildiMi: true, baslik: "Kahve İç"},
-        { yapildiMi: true, baslik: "İlacını Al"}
+        { yapildiMi: false, baslik: "React Çalış" },
+        { yapildiMi: false, baslik: "Konsere Git" },
+        { yapildiMi: true, baslik: "Kahve İç" },
+        { yapildiMi: true, baslik: "İlacını Al" }
       ]
     };
+    this.ekle = this.ekle.bind(this);
+  }
+
+  ekle(event) {
+    event.preventDefault();
+
+    const baslik = this.state.baslik.trim();
+    if (baslik == "") return;
+
+    const yeni = {
+      yapildiMi: false,
+      baslik: baslik
+    };
+
+    this.setState({
+      baslik: "",
+      yapilacaklar: sirala(this.state.yapilacaklar.concat(yeni))
+    });
+  }
+
+  sil(indeks, event) {
+    event.preventDefault();
+    const yeniYapilacaklar = [...this.state.yapilacaklar];
+    yeniYapilacaklar.splice(indeks, 1);
+    this.setState({ yapilacaklar: yeniYapilacaklar });
+  }
+
+  guncelle(indeks, event) {
+    const yeniYapilacaklar = [...this.state.yapilacaklar];
+    yeniYapilacaklar[indeks].yapildiMi = event.target.checked;
+    this.setState({
+      yapilacaklar: sirala(yeniYapilacaklar)
+    });
   }
 
   render() {
     return (
       <div className="TodoList">
         <h1>Yapacaklarım</h1>
-        <form>
-          <input type="text" placeholder="Ne yapacaksın?"
-            value={this.state.baslik}  required
-            onChange={(e) => this.setState({baslik: e.target.value})} />{" "}
-          <button>Ekle</button>
+        <form onSubmit={this.ekle}>
+          <input className="metin-kutu" name="okul" type="text" placeholder="Ne yapacaksın?"
+            value={this.state.baslik} required
+            onChange={(e) => this.setState({ baslik: e.target.value })} />{" "}
+          <button className="dugme">Ekle</button>
         </form>
-        <div>
+        <div className="liste">
           {
-            this.state.yapilacaklar.map((oge, indeks) => 
-              <div>
-                <input type="checkbox" checked={oge.yapildiMi} />
-                <span>{oge.baslik}</span>
+            this.state.yapilacaklar.map((oge, indeks) =>
+              <div className="liste-oge" key={indeks}>
+                <input className="liste-durum" type="checkbox" checked={oge.yapildiMi} onChange={(e) => this.guncelle(indeks, e)} />
+                <span className="liste-baslik">{oge.baslik}</span>
+                <a className="liste-sil" href="#"
+                  onClick={(e) => this.sil(indeks, e)}>&times;</a>
               </div>
             )
           }
@@ -38,6 +73,11 @@ export class TodoList extends Component {
       </div>
     );
   }
+}
+
+function sirala(liste) {
+  liste.sort((a, b) => a.yapildiMi - b.yapildiMi);
+  return liste;
 }
 
 export default TodoList;
